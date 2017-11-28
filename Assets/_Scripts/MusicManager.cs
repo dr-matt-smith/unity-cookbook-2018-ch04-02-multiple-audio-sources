f3de15ce0fabf49775e81fb933d668115c4df552
dr-matt-smith <dr_matt_smith@me.com>
2017-11-28 10:59:42 +0000

    updated code to match following recipe improved identifiers

---
+++ Assets/_Scripts/MusicManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour 
+{
+    // audio source component 1
+    public AudioSource audioSourceMedieval;
+
+    // audio source component 2
+    public AudioSource audioSourceArcade;
+
+    // every frame pause/resume music clip if correct key is pressed
+    void Update()
+    {
+        // Music 1
+        // if already started, resume playing
+        // else start playing
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (audioSourceMedieval.time > 0)
+                audioSourceMedieval.UnPause();
+            else
+                audioSourceMedieval.Play();
+        }
+
+        // pause playing
+        if (Input.GetKey(KeyCode.LeftArrow))
+            audioSourceMedieval.Pause();
+
+        // Music 2
+        // if already started, resume playing
+        // else start playing
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (audioSourceArcade.time > 0)
+                audioSourceArcade.UnPause();
+            else
+                audioSourceArcade.Play();
+        }
+
+        // pause playing
+        if (Input.GetKey(KeyCode.DownArrow))
+            audioSourceArcade.Pause();
+
+    }
+}
